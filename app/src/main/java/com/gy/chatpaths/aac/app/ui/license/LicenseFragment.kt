17ae268557a9280c1ceb69387f0e6d784321bd41
Gary Yund <gary.yund@gmail.com>
2023-04-07 01:11:04 -0400

    add license activity

--- app/src/main/java/com/gy/chatpaths/aac/app/ui/license/LicenseFragment.kt
+++ app/src/main/java/com/gy/chatpaths/aac/app/ui/license/LicenseFragment.kt
@@ -1,19 +1,31 @@
 package com.gy.chatpaths.aac.app.ui.license
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.gy.chatpaths.aac.app.R
+import com.gy.chatpaths.aac.app.databinding.FragmentLicenseBinding
 
 class LicenseFragment : Fragment() {
-
+    private var binder: FragmentLicenseBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_license, container, false)
+        binder = FragmentLicenseBinding.inflate(inflater, container, false)
+        binder!!.button.setOnClickListener {
+            launchLicenseActivity()
+        }
+
+        return binder!!.root
+    }
+
+    fun launchLicenseActivity() {
+        startActivity(Intent(context, OssLicensesMenuActivity::class.java))
     }
 }
