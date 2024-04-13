8d16ed37849086d87e39c40757413f407d11a37a
Gary Yund <gary.yund@gmail.com>
2024-04-13 12:50:08 -0400

    fix test

--- app/src/androidTest/java/com/gy/chatpaths/aac/app/model/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gy/chatpaths/aac/app/model/ExampleInstrumentedTest.kt
@@ -16,6 +16,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.gy.chatpaths.aac.data.test", appContext.packageName)
+        assertEquals("com.gy.chatpaths.aac.app", appContext.packageName)
     }
 }
